80aaefc640da472dc909934f2ceefb4c0d6fa90d
Brendan Fernandes <128330177+brendanfernandes-projects@users.noreply.github.com>
2024-03-14 21:53:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import os
 load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 Bootstrap5(app)
 
 TMDB_APIKEY = os.getenv("API_KEY")
